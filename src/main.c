d5f8804d5e60e57e41edf271e137a99d9be6ac57
Alex B <a01712835@tec.mx>
2025-05-13 14:23:30 -0600

    Correct main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "raylib.h"
-#include <stdio.h>
 
 int main() {
   InitWindow(800, 400, "Hello World");
 
   while (!WindowShouldClose()) {
+    BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Hello World!", 0, 0, 16, DARKGRAY);
+    EndDrawing();
   }
 
   CloseWindow();
